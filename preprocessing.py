e28a03b1b193b39ef00eda27a9541202d7b5bade
billymcross <billy.m.cross@gmail.com>
2021-10-07 18:38:23 -0400

    starting preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,12 @@
 import numpy as np
+from matplotlib import pyplot as plot
 
 image_data = np.load('images.npy')
+
+image_data_list = []
+
+for i in range(0, len(image_data), 28):
+    image_data_list.append(image_data[i:28])
+
+plot.imshow(image_data_list[0], cmap='gray')
+plot.show()
